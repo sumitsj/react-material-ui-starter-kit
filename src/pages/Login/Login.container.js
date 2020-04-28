beb6c394a1074b99870d9169e39903f182ee146a
Sumit Jadhav <summit.jadhav@gmail.com>
2020-04-29 01:45:26 +0530

    Added tests for Login Container component.

--- src/pages/Login/Login.container.js
+++ src/pages/Login/Login.container.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Login from './Login.view';
 
 const LoginContainer = () => {
   const isAuthenticated = localStorage.getItem('auth-token');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [showPassword, setShowPassword] = React.useState(false);
 
   const props = {
     username,
